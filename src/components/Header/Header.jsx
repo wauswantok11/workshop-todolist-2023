8c1e8a4ef705f6a6d1e3a4da428ba5cb3e1cafbf
soncomqiq <idgamescs@hotmail.com>
2023-05-12 09:50:21 +0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,17 @@ import "./Header.css"
 
 function Header() {
     return (
-        <div>This is Header</div>
+        <div className="container-header">
+            <div className="label-header">
+                Filter:
+            </div>
+            <select class="select-header">
+                <option>2022</option>
+                <option>2023</option>
+                <option>2024</option>
+            </select>
+        </div>
+
     )
 }
 
