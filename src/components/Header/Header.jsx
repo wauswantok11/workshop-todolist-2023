d6653edb29edf06295d2e0080d18b433ee4c4aba
soncomqiq <idgamescs@hotmail.com>
2023-05-12 12:53:58 +0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header({ value, onChange }) {
             <div className="label-header">
                 Filter:
             </div>
-            <select value={value} onChange={onChange} class="select-header">
+            <select value={value} onChange={onChange} className="select-header">
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
